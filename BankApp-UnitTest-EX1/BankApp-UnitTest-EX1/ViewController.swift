9e40cd1c3803cd5097af257eaac81a70fe39e9b6
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-08 16:54:52 +0200

    first commit

---
+++ BankApp-UnitTest-EX1/BankApp-UnitTest-EX1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp-UnitTest-EX1
+//
+//  Created by eidd5180 on 08/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
