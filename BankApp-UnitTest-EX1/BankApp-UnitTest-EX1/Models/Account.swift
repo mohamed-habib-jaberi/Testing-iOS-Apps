e0e4f816c577d5a66dc685a2b1cc701505c42fb9
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-08 17:03:18 +0200

    write first unit test

---
+++ BankApp-UnitTest-EX1/BankApp-UnitTest-EX1/Models/Account.swift
@@ -0,0 +1,12 @@
+//
+//  Account.swift
+//  BankApp-UnitTest-EX1
+//
+//  Created by eidd5180 on 08/07/2022.
+//
+
+import Foundation
+
+struct Account {
+    var balance: Double = 0.0
+}
--- BankApp-UnitTest-EX1/BankApp-UnitTest-EX1Tests/BankApp_UnitTest_EX1Tests.swift
+++ BankApp-UnitTest-EX1/BankApp-UnitTest-EX1Tests/BankApp_UnitTest_EX1Tests.swift
@@ -6,30 +6,13 @@
 //
 
 import XCTest
+@testable import BankApp_UnitTest_EX1
 
 class BankApp_UnitTest_EX1Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_InitialBalanceZero() {
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let account = Account()
+        XCTAssertTrue(account.balance == 0, "Balance is not zero!")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
