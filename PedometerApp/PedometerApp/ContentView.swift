1866f4f871199da9d7bf400de47649125a70daf7
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-13 14:32:32 +0200

    PedometerApp initial commit

---
+++ PedometerApp/PedometerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PedometerApp
+//
+//  Created by eidd5180 on 13/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PedometerApp/PedometerApp/PedometerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerAppApp.swift
+//  PedometerApp
+//
+//  Created by eidd5180 on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PedometerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
