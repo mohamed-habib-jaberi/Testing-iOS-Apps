a89155dcb5fbd894665ef97e48f193abeed69537
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-13 13:23:07 +0200

    Testing asynchronous code WRONG WAY

---
+++ TestingAsyncUsingExpectations/TestingAsyncUsingExpectations/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  TestingAsyncUsingExpectations
+//
+//  Created by eidd5180 on 13/07/2022.
+//
+
+import Foundation
+
+struct Post: Codable {
+    let title: String
+}
+
---
+++ TestingAsyncUsingExpectations/TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAsyncUsingExpectationsApp.swift
+//  TestingAsyncUsingExpectations
+//
+//  Created by eidd5180 on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingAsyncUsingExpectationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
