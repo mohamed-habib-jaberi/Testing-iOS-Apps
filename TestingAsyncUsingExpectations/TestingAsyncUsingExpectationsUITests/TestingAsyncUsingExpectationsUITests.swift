8330157ffc57e8eb0c283bda3894c65be146cd85
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-13 13:32:13 +0200

    Testing asynchronous code WRONG WAY

--- TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsUITests/TestingAsyncUsingExpectationsUITests.swift
+++ TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsUITests/TestingAsyncUsingExpectationsUITests.swift
@@ -9,5 +9,24 @@ import XCTest
 @testable import TestingAsyncUsingExpectations
 
 class TestingAsyncUsingExpectationsTests: XCTestCase {
+    func test_GetAllPosts() {
+
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            guard let data = data, error == nil else {
+                XCTFail()
+                return
+            }
+
+            let posts =  try! JSONDecoder().decode([Post].self, from: data)
+            XCTAssertTrue(posts.count > 0)
+
+        }.resume()
+
+        // NO ASSERT
+
+    }
 
 }
