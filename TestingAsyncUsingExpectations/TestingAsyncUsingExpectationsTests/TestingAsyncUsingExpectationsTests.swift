357568f20b5f445bb1c5b39e58a834b98854497a
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-13 13:35:05 +0200

    fix

--- TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsTests/TestingAsyncUsingExpectationsTests.swift
+++ TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsTests/TestingAsyncUsingExpectationsTests.swift
@@ -9,28 +9,24 @@ import XCTest
 @testable import TestingAsyncUsingExpectations
 
 class TestingAsyncUsingExpectationsTests: XCTestCase {
+    func test_GetAllPosts() {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        URLSession.shared.dataTask(with: url) { data, response, error in
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+            guard let data = data, error == nil else {
+                XCTFail()
+                return
+            }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+            let posts =  try! JSONDecoder().decode([Post].self, from: data)
+            XCTAssertTrue(posts.count > 0)
+
+        }.resume()
+
+        // NO ASSERT
+
     }
 
 }
--- TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsUITests/TestingAsyncUsingExpectationsUITests.swift
+++ TestingAsyncUsingExpectations/TestingAsyncUsingExpectationsUITests/TestingAsyncUsingExpectationsUITests.swift
@@ -5,28 +5,3 @@
 //  Created by eidd5180 on 13/07/2022.
 //
 
-import XCTest
-@testable import TestingAsyncUsingExpectations
-
-class TestingAsyncUsingExpectationsTests: XCTestCase {
-    func test_GetAllPosts() {
-
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-
-            guard let data = data, error == nil else {
-                XCTFail()
-                return
-            }
-
-            let posts =  try! JSONDecoder().decode([Post].self, from: data)
-            XCTAssertTrue(posts.count > 0)
-
-        }.resume()
-
-        // NO ASSERT
-
-    }
-
-}
