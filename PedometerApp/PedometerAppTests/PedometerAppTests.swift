0019144dabe2e3e92550ba37145dae85ca8cbdb9
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-13 14:36:19 +0200

    Testing the CMPedometer (WRONG WAY)

--- PedometerApp/PedometerAppTests/PedometerAppTests.swift
+++ PedometerApp/PedometerAppTests/PedometerAppTests.swift
@@ -6,31 +6,30 @@
 //
 
 import XCTest
-@testable import PedometerApp
+import CoreMotion
 
 class PedometerAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_CMPedometer_LoadingHistorialData() {
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        let now = Date()
+        var data: CMPedometerData?
+        let then = now.addingTimeInterval(-1000)
+        let exp = expectation(description: "Pedometer query returns...")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        let pedometer = CMPedometer()
+        pedometer.queryPedometerData(from: now, to: then) { (pedometerData, error) in
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            data = pedometerData
+            exp.fulfill()
         }
+
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNotNil(data)
+        if let steps = data?.numberOfSteps {
+            XCTAssertTrue(steps.intValue > 0)
+        }
+
     }
 
 }
