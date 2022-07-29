14c183b2a449d8c969567a309b6aed47ae11af4f
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2022-07-29 16:17:00 +0200

    writting the acceptance test

--- AppleCard/AppleCardAcceptanceTests/Steps/UnderAgeApplyingCardSteps.swift
+++ AppleCard/AppleCardAcceptanceTests/Steps/UnderAgeApplyingCardSteps.swift
@@ -14,15 +14,39 @@ class UnderAgeApplyingCardSteps {
     func run() {
         
         Given("the floiowing user information") { args, userInfo in
-            
+            XCUIApplication().launch()
+
+            let rows: NSArray = userInfo["DataTable"] as NSArray
+            for index in 1..<rows.count{
+
+                let data = rows[ index] as! NSArray
+                let name = data[0] as! String
+                let ssn = data[1] as! String
+                let dob = data[2] as! String
+
+                let nameTextField = XCUIApplication().textFields["nameTextField"]
+                nameTextField.tap()
+                nameTextField.typeText(name + "\n" )
+
+                let ssnTextField = XCUIApplication().textFields["ssnTextField"]
+                ssnTextField.tap()
+                ssnTextField.typeText(ssnTextField + "\n" )
+
+                let dobTextField = XCUIApplication().textFields["dobTextField"]
+                dobTextField.tap()
+                dobTextField.typeText(dob + "\n" )
+
+
+            }
         }
         
         When("I press the apply button") { _, _ in
-            
+            XCUIApplication().buttons["applyButton"].tap()
         }
         
         Then("I should get a rejection message") { _,_ in
-            
+            let messageLabel = XCUIApplication().staticTexts[ "messageLabel"]
+            XCTAssertEqual("Denied : underage", messageLabel.label)
         }
     }
 }
